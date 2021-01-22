806a602121eae32d4e846dc3b1316a3a16706865
Ethan Zimbelman <itszimbo@gmail.com>
2021-01-22 10:12:38 -0800

    polishing tic tac toe implementation

--- src/Board.js
+++ src/Board.js
@@ -3,16 +3,14 @@ import Square from './Square';
 import Banner from './Banner';
 import { calculateWinner } from './helpers';
 
-// React Hooks
 export default function Board() {
-  // useState
   const [ squares, setSquares ] = useState(Array(9).fill(null));
   const [ winner, setWinner ] = useState(null);
   const [ xIsNext, setXIsNext ] = useState(true);
 
-  const status = 'Next player: X';
-
   const handleClick = (i) => {
+    if (winner) return;
+
     const squareCopy = squares.slice();
     squareCopy[i] = xIsNext ? 'X' : 'O';
     setSquares(squareCopy);
@@ -22,13 +20,13 @@ export default function Board() {
 
   const renderSquare = (i) => {
     return <Square
-      value={squares[i]} 
+      value={squares[i]}
       onClick={() => handleClick(i)} />
   };
 
   return (
     <div>
-      <div className="status" onClick={() => setSquares("X")}>{status}</div>
+      <div className="status">{`Next player: ${xIsNext ? 'X' : 'O'}`}</div>
       {
         winner ? <Banner name={winner} /> : null
       }
@@ -50,61 +48,3 @@ export default function Board() {
     </div>
   );
 }
-
-/**
- * CLASS VERSION OF THE BOARD COMPONENT
- */
-
-// class Board extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       squares: Array(9).fill(null),
-//       winner: null,
-//       xIsNext: true
-//     };
-//   }
-
-//   handleClick(i) {
-//     const squares = this.state.squares.slice();
-//     squares[i] = this.state.xIsNext ? 'X' : 'O';
-//     const winner = calculateWinner(squares);
-//     this.setState({squares: squares, winner: winner, xIsNext: !this.state.xIsNext});
-//   }
-
-//   renderSquare(i) {
-//     const clicker = () => this.handleClick(i);
-//     return <Square
-//       value={this.state.squares[i]} 
-//       onClick={clicker} />
-//   }
-
-//   render() {
-//     const status = 'Next player: X';
-//     // Can just write javascript here
-//     return (
-//       <div>
-//         {
-//           this.state.winner ? <Banner name={this.state.winner} /> : null
-//         }
-//         <div className="status">{status}</div>
-//         <div className="board-row">
-//           {this.renderSquare(0)}
-//           {this.renderSquare(1)}
-//           {this.renderSquare(2)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(3)}
-//           {this.renderSquare(4)}
-//           {this.renderSquare(5)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(6)}
-//           {this.renderSquare(7)}
-//           {this.renderSquare(8)}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
